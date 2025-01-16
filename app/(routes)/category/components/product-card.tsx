48812cc786e3357c6bce1c898352326d5f75ae14
Sergio Navarro <nachonavarro197@gmail.com>
2025-01-16 17:54:53 -0300

    arreglo en product-card

--- app/(routes)/category/components/product-card.tsx
+++ app/(routes)/category/components/product-card.tsx
@@ -17,13 +17,13 @@ const ProductCard = (props: ProductCardProps) => {
     const router = useRouter();
 
     return (
-        <Link href={`/product/${product.slug}`}
+        <Link href={`/product/${product.attributes.slug}`}
             className="relative p-2 transition-all duration-100 rounded-lg hover:shadow-md">
             <div className="absolute flex items-center justify-between gap-3 px-2 z-[1] top-4">
                 <p
-                    className="px-2 py-1 text-xs text-white bg-black rounded-full dark:bg-white dark:text-black w-fit">{product.productName}</p>
+                    className="px-2 py-1 text-xs text-white bg-black rounded-full dark:bg-white dark:text-black w-fit">{product.attributes.productName}</p>
                 <p
-                    className="px-2 py-1 text-xs text-white bg-purple-900 rounded-full dark:bg-white dark:text-black w-fit">${ formatPrice(product.price) }</p>
+                    className="px-2 py-1 text-xs text-white bg-purple-900 rounded-full dark:bg-white dark:text-black w-fit">${ formatPrice(product.attributes.price) }</p>
             </div>
             <Carousel
                 opts={{
@@ -32,15 +32,15 @@ const ProductCard = (props: ProductCardProps) => {
                 className="w-full max-w-sm"
             >
                 <CarouselContent>
-                    {product.images.map((image) => (
+                    {product.attributes.images.data.map((image) => (
                         <CarouselItem key={image.id} className="group">
-                            <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${image.url}`}
-                                alt={product.productName}
+                            <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${image.attributes.url}`}
+                                alt={product.attributes.productName}
                                 className="rounded-xl"
                             />
                             <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
                                 <div className="flex justify-center gap-x-6">
-                                    <IconButton onClick={()=> router.push(`/product/${product.slug}`)} icon={<Expand size={20} className="text-gray-600"/>}/>
+                                    <IconButton onClick={()=> router.push(`/product/${product.attributes.slug}`)} icon={<Expand size={20} className="text-gray-600"/>}/>
                                     <IconButton onClick={()=> console.log("product")} icon={<ShoppingCart size={20} className="text-gray-600"/>}/>
                                 </div>
                             </div>
@@ -48,8 +48,8 @@ const ProductCard = (props: ProductCardProps) => {
                     ))}
                 </CarouselContent>
             </Carousel>
-            <p className="text-2xl text-center">{product.productName}</p>
-            <p className="font-bold text-center">${formatPrice(product.price)}</p>
+            <p className="text-2xl text-center">{product.attributes.productName}</p>
+            <p className="font-bold text-center">${formatPrice(product.attributes.price)}</p>
         </Link>
     )
 }
